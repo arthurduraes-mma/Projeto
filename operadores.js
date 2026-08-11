a4f219b2e7c50b4bf1a3c27db3f919b4ffe26944
arthurduraes-mma <arthur.duraes@edu.senai.br>
2026-08-11 14:15:44 -0300

    Update operadores.js

--- operadores.js
+++ operadores.js
@@ -4,3 +4,23 @@
 // Divisão / 10 /2 = 5
 // Resto (Módulo) % 10 % 3 = 1
 // potencia ** 2 ** 3 = 8
+
+
+let idade = 20;
+
+console.log(idade > 10);
+console.log(10 > 15);
+
+let nota = 6;
+
+console.log(nota < 7);
+
+console.log(idade > 10 && nota < 7);
+console.log(idade > 10 || nota > 7);
+console.log(!(idade > 10));
+// Maior que >
+//Menor que <
+// Menor ou igual <=
+// Igual ==
+// Estritamente igual ===
+// Difre
